12f84eb2e0788142a7fd4be51c5cf3bf6ab65ec3
Matthew Glazar <strager@fb.com>
2014-01-02 19:49:06 -0800

    failing fiber tests

--- test/TestFiber.cc
+++ test/TestFiber.cc
@@ -1,34 +1,169 @@
 #include <B/Fiber.h>
+#include <B/Internal/GTest.h>
 
 #include <gtest/gtest.h>
 
 TEST(TestFiber, ForkNotExecutedImmediately) {
-    struct B_Exception *ex;
-
     B_FiberContext *fiber_context;
-    ex = b_fiber_context_allocate(&fiber_context);
-    ASSERT_EQ(nullptr, ex);
+    B_CHECK_EX(b_fiber_context_allocate(&fiber_context));
 
-    bool fork_executed = false;
-    ex = b_fiber_context_fork(
+    bool volatile fork_executed = false;
+    B_CHECK_EX(b_fiber_context_fork(
         fiber_context,
-        [](void *user_closure) -> void * {
-            bool *fork_executed
-                = static_cast<bool *>(user_closure);
-            *fork_executed = true;
-            return nullptr;
-        },
-        &fork_executed,
-        nullptr);  // out_callback_result
-    ASSERT_EQ(nullptr, ex);
+        [&fork_executed]() {
+            fork_executed = true;
+        }));
 
     EXPECT_FALSE(fork_executed);
 
-    ex = b_fiber_context_finish(fiber_context);
-    ASSERT_EQ(nullptr, ex);
+    B_CHECK_EX(b_fiber_context_finish(fiber_context));
 
     EXPECT_TRUE(fork_executed);
 
-    ex = b_fiber_context_deallocate(fiber_context);
-    ASSERT_EQ(nullptr, ex);
+    B_CHECK_EX(b_fiber_context_deallocate(fiber_context));
+}
+
+TEST(TestFiber, ThreeYieldingForksFairlyScheduled) {
+    B_FiberContext *fiber_context;
+    B_CHECK_EX(b_fiber_context_allocate(&fiber_context));
+
+    int volatile main_scheduled = 0;
+    int volatile fork1_scheduled = 0;
+    int volatile fork2_scheduled = 0;
+
+    B_CHECK_EX(b_fiber_context_fork(
+        fiber_context,
+        [fiber_context, &fork1_scheduled]() {
+            ++fork1_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork1_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork1_scheduled;
+        }));
+
+    B_CHECK_EX(b_fiber_context_fork(
+        fiber_context,
+        [fiber_context, &fork2_scheduled]() {
+            ++fork2_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork2_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork2_scheduled;
+        }));
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(1, main_scheduled);
+    EXPECT_EQ(1, fork1_scheduled);
+    EXPECT_EQ(1, fork2_scheduled);
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(2, main_scheduled);
+    EXPECT_EQ(2, fork1_scheduled);
+    EXPECT_EQ(2, fork2_scheduled);
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(3, main_scheduled);
+    EXPECT_EQ(3, fork1_scheduled);
+    EXPECT_EQ(3, fork2_scheduled);
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(4, main_scheduled);
+    EXPECT_EQ(3, fork1_scheduled);
+    EXPECT_EQ(3, fork2_scheduled);
+
+    B_CHECK_EX(b_fiber_context_deallocate(fiber_context));
+}
+
+TEST(TestFiber, FourYieldingForksFairlyScheduled) {
+    B_FiberContext *fiber_context;
+    B_CHECK_EX(b_fiber_context_allocate(&fiber_context));
+
+    int volatile main_scheduled = 0;
+    int volatile fork1_scheduled = 0;
+    int volatile fork2_scheduled = 0;
+    int volatile fork3_scheduled = 0;
+
+    B_CHECK_EX(b_fiber_context_fork(
+        fiber_context,
+        [fiber_context, &fork1_scheduled]() {
+            ++fork1_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork1_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork1_scheduled;
+        }));
+
+    B_CHECK_EX(b_fiber_context_fork(
+        fiber_context,
+        [fiber_context, &fork2_scheduled]() {
+            ++fork2_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork2_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork2_scheduled;
+        }));
+
+    B_CHECK_EX(b_fiber_context_fork(
+        fiber_context,
+        [fiber_context, &fork3_scheduled]() {
+            ++fork3_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork3_scheduled;
+
+            B_CHECK_EX(b_fiber_context_hard_yield(
+                fiber_context));
+            ++fork3_scheduled;
+        }));
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(1, main_scheduled);
+    EXPECT_EQ(1, fork1_scheduled);
+    EXPECT_EQ(1, fork2_scheduled);
+    EXPECT_EQ(1, fork3_scheduled);
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(2, main_scheduled);
+    EXPECT_EQ(2, fork1_scheduled);
+    EXPECT_EQ(2, fork2_scheduled);
+    EXPECT_EQ(2, fork3_scheduled);
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(3, main_scheduled);
+    EXPECT_EQ(3, fork1_scheduled);
+    EXPECT_EQ(3, fork2_scheduled);
+    EXPECT_EQ(3, fork3_scheduled);
+
+    ++main_scheduled;
+    B_CHECK_EX(b_fiber_context_hard_yield(fiber_context));
+    EXPECT_EQ(4, main_scheduled);
+    EXPECT_EQ(3, fork1_scheduled);
+    EXPECT_EQ(3, fork2_scheduled);
+    EXPECT_EQ(3, fork3_scheduled);
+
+    B_CHECK_EX(b_fiber_context_deallocate(fiber_context));
 }
