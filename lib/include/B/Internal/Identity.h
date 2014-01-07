54219eab2df58ff13d6ab6a538e25825475b4b04
Matthew Glazar <strager@fb.com>
2014-01-06 19:04:32 -0800

    wip identity for sockets

--- lib/include/B/Internal/Identity.h
+++ lib/include/B/Internal/Identity.h
@@ -1,12 +1,16 @@
 #ifndef IDENTITY_H_1D83BD3B_407B_466A_B7CD_51C87B765678
 #define IDENTITY_H_1D83BD3B_407B_466A_B7CD_51C87B765678
 
+#include <B/Internal/Common.h>
+
 #include <stdbool.h>
 #include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct B_FiberContext;
 
 struct B_Identity {
     char const *const data;
@@ -18,6 +22,12 @@ b_identity_allocate(
     char const *data,
     size_t data_size);
 
+struct B_Identity *
+b_identity_allocate_fiber_id(
+    void *context_zmq,
+    struct B_FiberContext *,
+    void *fiber_id);
+
 void
 b_identity_deallocate(
     struct B_Identity *);
@@ -27,11 +37,10 @@ b_identity_equal(
     struct B_Identity const *,
     struct B_Identity const *);
 
-#if 0
-struct B_Identity *
-b_identity_allocate_random(
-    void);
-#endif
+B_ERRFUNC
+b_identity_set_for_socket(
+    void *socket_zmq,
+    struct B_Identity const *);
 
 #ifdef __cplusplus
 }
--- lib/src/Internal/Identity.c
+++ lib/src/Internal/Identity.c
@@ -1,7 +1,11 @@
+#include <B/Exception.h>
 #include <B/Internal/Allocate.h>
 #include <B/Internal/Identity.h>
 #include <B/Internal/Portable.h>
 
+#include <zmq.h>
+
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -21,6 +25,56 @@ b_identity_allocate(
     });
 }
 
+struct B_Identity *
+b_identity_allocate_fiber_id(
+    void *context_zmq,
+    struct B_FiberContext *fiber_context,
+    void *fiber_id) {
+
+    // 0xFE [context_zmq] [fiber_context] [fiber_id]
+    // Endianness doesn't matter.
+    // ZeroMQ reserves 0x00 as the first byte.  We
+    // arbitrarily chose 0xFE.
+    uint8_t const magic = 0xFE;
+
+    enum {
+        data_size
+            = sizeof(magic)
+                + sizeof(context_zmq)
+                + sizeof(fiber_context)
+                + sizeof(fiber_id),
+    };
+    _Static_assert(
+        data_size <= 255,
+        "Generated identity must not exceed 255 bytes (ZeroMQ limit)");
+
+    uint8_t *data = malloc(data_size);
+    if (!data) {
+        return NULL;
+    }
+
+    uint8_t *p = data;
+
+    memcpy(p, &magic, sizeof(magic));
+    p += sizeof(magic);
+
+    memcpy(p, &context_zmq, sizeof(context_zmq));
+    p += sizeof(context_zmq);
+
+    memcpy(p, &fiber_context, sizeof(fiber_context));
+    p += sizeof(fiber_context);
+
+    memcpy(p, &fiber_id, sizeof(fiber_id));
+    p += sizeof(fiber_id);
+
+    assert(p == data + data_size);
+
+    B_ALLOCATE_RETURN(struct B_Identity, {
+        .data = (char const *) data,
+        .data_size = data_size,
+    });
+}
+
 void
 b_identity_deallocate(
     struct B_Identity *identity) {
@@ -36,4 +90,21 @@ b_identity_equal(
 
     return x->data_size == y->data_size
         && memcmp(x->data, y->data, x->data_size) == 0;
+}
+
+B_ERRFUNC
+b_identity_set_for_socket(
+    void *socket_zmq,
+    struct B_Identity const *identity) {
+
+    int rc = zmq_setsockopt(
+        socket_zmq,
+        ZMQ_IDENTITY,
+        identity->data,
+        identity->data_size);
+    if (rc != 0) {
+        return b_exception_errno("zmq_setsockopt", errno);
+    }
+
+    return NULL;
 }
