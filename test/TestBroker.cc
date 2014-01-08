93aa38d8c4612a4f06c2eafb01be64912b1287dd
Matthew Glazar <strager@fb.com>
2014-01-07 20:02:29 -0800

    more testz

--- test/TestBroker.cc
+++ test/TestBroker.cc
@@ -427,9 +427,9 @@ TEST(TestBroker, WorkerExitsWithoutWork) {
 // 2. Create worker A.
 // 3. Send work to broker.
 // 4. Create worker B.
-// 5. Worker A abandons work.
+// 5. Worker A exits and abandons work.
 // 6. Assert that worker B receives work.
-TEST(TestBroker, AbandoningGivesWorkToOtherWorker) {
+TEST(TestBroker, AbandoningGivesWorkToOtherNewWorker) {
     B_Exception *ex = NULL;
 
     static B_UUID const question_uuid
@@ -467,7 +467,7 @@ TEST(TestBroker, AbandoningGivesWorkToOtherWorker) {
     B_CHECK_EX(ex);
 
     // FIXME(strager): This method sucks!
-    B_LOG(B_INFO, "Waiting for worker to pick up worker READY.");
+    B_LOG(B_INFO, "Waiting for worker to pick up worker A READY.");
     sleep(1);
 
     // Send client request.
@@ -592,5 +592,208 @@ TEST(TestBroker, AbandoningGivesWorkToOtherWorker) {
         question_payload,
         sizeof(question_payload),
         worker_b_broker_dealer,
+        0);  // flags
+}
+
+// Test with two workers.
+//
+// 1. Create broker.
+// 2. Create worker A.
+// 4. Create worker B.
+// 3. Send work to broker.
+// 5. Worker A or B exits and abandons work.
+// 6. Assert that worker B or A receives work.
+TEST(TestBroker, AbandoningGivesWorkToOtherOldWorker) {
+    B_Exception *ex = NULL;
+
+    static B_UUID const question_uuid
+        = B_UUID("F5114112-F33B-4A48-882C-CB1E5393A471");
+    static uint8_t const question_payload[] = "hello world";
+
+    void *context_zmq = zmq_ctx_new();
+    ASSERT_NE(nullptr, context_zmq);
+    B_ZMQContextScope context_zmq_scope(context_zmq);
+
+    B_BrokerAddress *broker_address;
+    B_CHECK_EX(create_broker_thread(
+        context_zmq,
+        &broker_address));
+    sleep(1);
+
+    B_LOG(B_INFO, "Creating worker A.");
+    void *worker_a_broker_dealer;
+    B_CHECK_EX(b_zmq_socket(
+        context_zmq,
+        ZMQ_DEALER,
+        &worker_a_broker_dealer));
+    B_ZMQSocketScope worker_a_broker_dealer_scope(
+        worker_a_broker_dealer);
+    B_CHECK_EX(b_protocol_connectbind_worker(
+        worker_a_broker_dealer,
+        broker_address,
+        B_CONNECT));
+
+    b_protocol_send_worker_command(
+        worker_a_broker_dealer,
+        B_WORKER_READY,
+        0,  // flags
+        &ex);
+    B_CHECK_EX(ex);
+
+    B_LOG(B_INFO, "Creating worker B.");
+    void *worker_b_broker_dealer;
+    B_CHECK_EX(b_zmq_socket(
+        context_zmq,
+        ZMQ_DEALER,
+        &worker_b_broker_dealer));
+    B_ZMQSocketScope worker_b_broker_dealer_scope(
+        worker_b_broker_dealer);
+    B_CHECK_EX(b_protocol_connectbind_worker(
+        worker_b_broker_dealer,
+        broker_address,
+        B_CONNECT));
+
+    b_protocol_send_worker_command(
+        worker_b_broker_dealer,
+        B_WORKER_READY,
+        0,  // flags
+        &ex);
+    B_CHECK_EX(ex);
+
+    // FIXME(strager): This method sucks!
+    B_LOG(B_INFO, "Waiting for worker to pick up workers A and B READY.");
+    sleep(1);
+
+    // Send client request.
+    void *client_broker_dealer;
+    B_CHECK_EX(b_zmq_socket(
+        context_zmq,
+        ZMQ_DEALER,
+        &client_broker_dealer));
+    B_ZMQSocketScope client_broker_dealer_scope(
+        client_broker_dealer);
+    B_CHECK_EX(b_protocol_connectbind_client(
+        client_broker_dealer,
+        broker_address,
+        B_CONNECT));
+
+    B_CHECK_EX(b_protocol_send_identity_delimiter(
+        client_broker_dealer,
+        ZMQ_SNDMORE));
+
+    {
+        B_RequestID request_id = {{0, 1, 2, 3}};
+        B_CHECK_EX(b_protocol_send_request_id(
+            client_broker_dealer,
+            &request_id,
+            ZMQ_SNDMORE));
+    }
+
+    b_protocol_send_uuid(
+        client_broker_dealer,
+        question_uuid,
+        ZMQ_SNDMORE,
+        &ex);
+    B_CHECK_EX(ex);
+
+    B_CHECK_EX(b_zmq_send(
+        client_broker_dealer,
+        question_payload,
+        sizeof(question_payload),
+        0));  // flags
+
+    B_LOG(B_INFO, "Waiting for either worker to receive request.");
+    void *abandoning_worker = NULL;
+    void *final_worker = NULL;
+    {
+        zmq_pollitem_t pollitems[] = {
+            { worker_a_broker_dealer, 0, ZMQ_POLLIN, 0 },
+            { worker_b_broker_dealer, 0, ZMQ_POLLIN, 0 },
+        };
+        while (!abandoning_worker && !final_worker) {
+            long const timeout_milliseconds = 1000;
+            int rc = zmq_poll(
+                pollitems,
+                sizeof(pollitems) / sizeof(*pollitems),
+                timeout_milliseconds);
+            if (rc == -1) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                FAIL()
+                    << "zmq_poll failed; errno = "
+                    << errno;
+            }
+
+            if (pollitems[0].revents & ZMQ_POLLIN) {
+                abandoning_worker = pollitems[0].socket;
+                final_worker = pollitems[1].socket;
+            }
+            if (abandoning_worker || final_worker) {
+                FAIL() << "Both workers received work.";
+            }
+            if (pollitems[1].revents & ZMQ_POLLIN) {
+                abandoning_worker = pollitems[1].socket;
+                final_worker = pollitems[0].socket;
+            }
+        }
+    }
+
+    // Exit, receive client request, and abandon.
+    b_protocol_send_worker_command(
+        abandoning_worker,
+        B_WORKER_EXIT,
+        0,  // flags
+        &ex);
+    B_CHECK_EX(ex);
+
+    b_protocol_send_worker_command(
+        abandoning_worker,
+        B_WORKER_ABANDON,
+        ZMQ_SNDMORE,
+        &ex);
+    B_CHECK_EX(ex);
+
+    B_CHECK_EX(b_protocol_recv_identity_delimiter(
+        abandoning_worker,
+        0));  // flags
+
+    B_CHECK_EX(b_zmq_copy(
+        abandoning_worker,
+        abandoning_worker,
+        0));  // flags
+
+    // Receive client request.
+    B_CHECK_EX(b_protocol_recv_identity_delimiter(
+        final_worker,
+        0));  // flags
+
+    B_Identity *client_identity
+        = b_protocol_recv_identity_envelope(
+            final_worker,
+            0,  // flags
+            &ex);
+    B_CHECK_EX(ex);
+    b_identity_deallocate(client_identity);
+
+    B_EXPECT_RECV_REQUEST_ID_EQ(
+        ((B_RequestID) {{0, 1, 2, 3}}),
+        final_worker);
+
+    {
+        B_UUID received_uuid = b_protocol_recv_uuid(
+            final_worker,
+            0,  // flags
+            &ex);
+        B_CHECK_EX(ex);
+
+        EXPECT_TRUE(
+            b_uuid_equal(question_uuid, received_uuid));
+    }
+
+    B_EXPECT_RECV(
+        question_payload,
+        sizeof(question_payload),
+        final_worker,
         0);  // flags
 }
