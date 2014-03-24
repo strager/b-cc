strager/b-cc

commit 4155680515067fa46681dc639ab433937f19fb26

Write test ensuring AnswerContext enqueues Question
Caught a bug in AnswerContext.

src/AnswerContext.c
@@ -379,9 +379,15 @@ need_queue_item_deallocate_(
         return false;
     }
 
-    (void) need_closure_release_(
-        need_queue_item->closure,
-        eh);
+    struct NeedClosure_ *need_closure
+        = need_queue_item->closure;
+    if (!need_closure->callback_called) {
+        // Ensure the callback is called so we don't leak
+        // resources.
+        (void) need_closure_cancel_(need_closure, eh);
+    }
+
+    (void) need_closure_release_(need_closure, eh);
     (void) b_deallocate(need_queue_item, eh);
     return true;
 }

test/TestAnswerContext.cc
@@ -1,0 +1,232 @@
+#include <B/AnswerContext.h>
+#include <B/Assert.h>
+#include <B/QuestionAnswer.h>
+#include <B/QuestionQueue.h>
+#include <B/RefCount.h>
+
+#include <memory>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+template<typename T>
+struct RemoveMockWrapper {
+    typedef T type;
+};
+
+template<typename TOther>
+struct RemoveMockWrapper<NiceMock<TOther>> {
+    typedef TOther type;
+};
+
+template<typename TOther>
+struct RemoveMockWrapper<NaggyMock<TOther>> {
+    typedef TOther type;
+};
+
+template<typename TOther>
+struct RemoveMockWrapper<StrictMock<TOther>> {
+    typedef TOther type;
+};
+
+template<typename T>
+static void
+MockRefCounting(
+        T &self) {
+    typedef typename RemoveMockWrapper<T>::type NonMock;
+
+    EXPECT_CALL(self, deallocate(_))
+        .WillRepeatedly(Invoke([&self](
+                B_ErrorHandler const *eh) {
+            bool should_deallocate = false;
+            if (!B_RELEASE(
+                    &self,
+                    &should_deallocate,
+                    eh)) {
+                return false;
+            }
+            EXPECT_FALSE(should_deallocate);
+            return true;
+        }));
+
+    EXPECT_CALL(self, replicate(_, _))
+        .WillRepeatedly(Invoke([&self](
+                NonMock **out,
+                B_ErrorHandler const *eh) {
+            if (!B_RETAIN(&self, eh)) {
+                return false;
+            }
+            *out = &self;
+            return true;
+        }));
+}
+
+class MockAnswer :
+        public B_AnswerClass<MockAnswer> {
+public:
+    B_REF_COUNTED_OBJECT;
+
+    MockAnswer() :
+            B_REF_COUNT_CONSTRUCTOR_INITIALIZER {
+    }
+
+    MOCK_CONST_METHOD3(equals, B_FUNC(
+        MockAnswer const &,
+        B_OUTPTR bool *,
+        B_ErrorHandler const *));
+
+    static B_FUNC
+    equal(
+            MockAnswer const &a,
+            MockAnswer const &b,
+            B_OUTPTR bool *out,
+            B_ErrorHandler const *eh) {
+        return a.equals(b, out, eh);
+    }
+
+    MOCK_CONST_METHOD2(replicate, B_FUNC(
+        B_OUTPTR MockAnswer **,
+        B_ErrorHandler const *));
+
+    MOCK_METHOD1(deallocate, B_FUNC(
+        B_ErrorHandler const *));
+
+    MOCK_CONST_METHOD2(serialize, B_FUNC(
+        B_OUT B_Serialized *,
+        B_ErrorHandler const *));
+
+    static B_FUNC
+    deserialize(
+            B_BORROWED B_Serialized,
+            B_OUTPTR MockAnswer **,
+            B_ErrorHandler const *) {
+        B_NYI();
+    }
+};
+
+class MockQuestion :
+        public B_QuestionClass<MockQuestion> {
+public:
+    typedef MockAnswer AnswerClass;
+    B_REF_COUNTED_OBJECT;
+
+    MockQuestion() :
+            B_REF_COUNT_CONSTRUCTOR_INITIALIZER {
+    }
+
+    MOCK_CONST_METHOD2(answer, B_FUNC(
+        B_OUTPTR MockAnswer **,
+        B_ErrorHandler const *));
+
+    MOCK_CONST_METHOD3(equals, B_FUNC(
+        MockQuestion const &,
+        B_OUTPTR bool *,
+        B_ErrorHandler const *));
+
+    static B_FUNC
+    equal(
+            MockQuestion const &a,
+            MockQuestion const &b,
+            B_OUTPTR bool *out,
+            B_ErrorHandler const *eh) {
+        return a.equals(b, out, eh);
+    }
+
+    MOCK_CONST_METHOD2(replicate, B_FUNC(
+        B_OUTPTR MockQuestion **,
+        B_ErrorHandler const *));
+
+    MOCK_METHOD1(deallocate, B_FUNC(
+        B_ErrorHandler const *));
+
+    MOCK_CONST_METHOD2(serialize, B_FUNC(
+        B_OUT B_Serialized *,
+        B_ErrorHandler const *));
+
+    static B_FUNC
+    deserialize(
+            B_BORROWED B_Serialized,
+            B_OUTPTR MockQuestion **,
+            B_ErrorHandler const *) {
+        B_NYI();
+    }
+};
+
+template<>
+B_UUID
+B_QuestionClass<MockQuestion>::uuid
+    = B_UUID_LITERAL("B7E06E83-B1D7-42BA-B9EA-EBA122B75697");
+
+// Ensures b_answer_context_need_one enqueues the question
+// on the QuestionQueue exactly once.
+TEST(TestAnswerContext, NeedOneEnqueues) {
+    B_ErrorHandler const *eh = nullptr;
+
+    // Must be alive while question_queue is destructed.
+    StrictMock<MockQuestion> question;
+    MockRefCounting(question);
+    StrictMock<MockQuestion> needed_question;
+    MockRefCounting(needed_question);
+    B_AnswerContext answer_context;
+
+    B_QuestionQueue *question_queue_raw;
+    ASSERT_TRUE(b_question_queue_allocate(
+        &question_queue_raw,
+        eh));
+    std::unique_ptr<B_QuestionQueue, B_QuestionQueueDeleter>
+        question_queue(question_queue_raw, eh);
+    question_queue_raw = nullptr;
+
+    answer_context.question = &question;
+    answer_context.question_vtable = MockQuestion::vtable();
+    answer_context.answer_callback = [](
+            B_Answer *,
+            void *,
+            B_ErrorHandler const *) {
+        // TODO(strager)
+        return false;
+    };
+    answer_context.answer_callback_opaque = nullptr;
+    answer_context.question_queue = question_queue.get();
+    answer_context.dependency_delegate = nullptr;
+
+    ASSERT_TRUE(b_answer_context_need_one(
+        &answer_context,
+        &needed_question,
+        MockQuestion::vtable(),
+        [](
+                B_TRANSFER B_Answer *,
+                B_ErrorHandler const *) {
+            return false;
+        },
+        [](
+                B_ErrorHandler const *) {
+            return false;
+        },
+        eh));
+
+    B_QuestionQueueItemObject *first_queue_item_raw;
+    ASSERT_TRUE(b_question_queue_try_dequeue(
+        question_queue.get(),
+        &first_queue_item_raw,
+        eh));
+    ASSERT_NE(nullptr, first_queue_item_raw);
+    std::unique_ptr<
+            B_QuestionQueueItemObject,
+            B_QuestionQueueItemDeleter>
+        first_queue_item(first_queue_item_raw, eh);
+    first_queue_item_raw = nullptr;
+
+    EXPECT_EQ(&needed_question, first_queue_item->question);
+    EXPECT_EQ(
+        MockQuestion::vtable(),
+        first_queue_item->question_vtable);
+
+    B_QuestionQueueItemObject *second_queue_item_raw;
+    ASSERT_TRUE(b_question_queue_try_dequeue(
+        question_queue.get(),
+        &second_queue_item_raw,
+        eh));
+    ASSERT_EQ(nullptr, second_queue_item_raw);
+}

include/B/QuestionQueue.h
@@ -52,6 +52,15 @@ b_question_queue_dequeue(
         B_OUTPTR struct B_QuestionQueueItemObject *B_OPT *,
         struct B_ErrorHandler const *);
 
+// Non-blocking.  Outputs a NULL pointer if
+// b_question_queue_close was called or if the queue is
+// empty.
+B_EXPORT_FUNC
+b_question_queue_try_dequeue(
+        struct B_QuestionQueue *,
+        B_OUTPTR struct B_QuestionQueueItemObject *B_OPT *,
+        struct B_ErrorHandler const *);
+
 // After this is called, b_question_queue_dequeue will
 // return NULL, and b_question_queue_enqueue will do
 // nothing.  Idempotent.
@@ -96,6 +105,19 @@ struct B_QuestionQueueDeleter :
             B_QuestionQueue *queue) {
         (void) b_question_queue_deallocate(
             queue,
+            this->error_handler);
+    }
+};
+
+struct B_QuestionQueueItemDeleter :
+        public B_Deleter {
+    using B_Deleter::B_Deleter;
+
+    void
+    operator()(
+            B_QuestionQueueItemObject *queue_item) {
+        (void) b_question_queue_item_object_deallocate(
+            queue_item,
             this->error_handler);
     }
 };

include/B/RefCount.h
@@ -24,6 +24,11 @@ struct B_RefCount {
     })
 #endif
 
+#if defined(__cplusplus)
+#define B_REF_COUNT_CONSTRUCTOR_INITIALIZER \
+    B_REF_COUNTED_NAME_(B_REF_COUNT_INITIALIZER)
+#endif
+
 #define B_REF_COUNTED_NAME_ _object_ref_count
 
 #define B_REF_COUNTED_OBJECT \

src/QuestionQueue.cc
@@ -78,6 +78,21 @@ struct B_QuestionQueue {
         }
     }
 
+    B_QuestionQueueItemObject *
+    try_dequeue() {
+        {
+            B_PthreadMutexHolder locker(&this->lock);
+
+            if (queue_items.empty()) {
+                return nullptr;
+            } else {
+                auto front = this->queue_items.front();
+                this->queue_items.pop_front();
+                return front;
+            }
+        }
+    }
+
     void
     close() {
         {
@@ -150,6 +165,18 @@ b_question_queue_dequeue(
     B_CHECK_PRECONDITION(eh, out);
 
     *out = queue->dequeue();
+    return true;
+}
+
+B_EXPORT_FUNC
+b_question_queue_try_dequeue(
+        struct B_QuestionQueue *queue,
+        B_OUTPTR struct B_QuestionQueueItemObject *B_OPT *out,
+        struct B_ErrorHandler const *eh) {
+    B_CHECK_PRECONDITION(eh, queue);
+    B_CHECK_PRECONDITION(eh, out);
+
+    *out = queue->try_dequeue();
     return true;
 }
 

commit cae468d65d5c27bcd290c72e7c5539c30f131381

Fix AnswerContext bug causing builds to fail
When a queue item was deallocated, the need() call was
cancelled.  This behaviour is incorrect; when the *need
closure* deallocates, the need() must be cancelled.

Bug was introduced in commit
4155680515067fa46681dc639ab433937f19fb26 "Write test
ensuring AnswerContext enqueues Question".

src/AnswerContext.c
@@ -379,15 +379,9 @@ need_queue_item_deallocate_(
         return false;
     }
 
-    struct NeedClosure_ *need_closure
-        = need_queue_item->closure;
-    if (!need_closure->callback_called) {
-        // Ensure the callback is called so we don't leak
-        // resources.
-        (void) need_closure_cancel_(need_closure, eh);
-    }
-
-    (void) need_closure_release_(need_closure, eh);
+    (void) need_closure_release_(
+        need_queue_item->closure,
+        eh);
     (void) b_deallocate(need_queue_item, eh);
     return true;
 }
@@ -457,7 +451,13 @@ need_closure_release_(
         return true;
     }
 
+    if (!need_closure->callback_called) {
+        // Ensure the callback is called so we don't leak
+        // resources.
+        (void) need_closure_cancel_(need_closure, eh);
+    }
     B_ASSERT(need_closure->callback_called);
+
     for (
             size_t i = 0;
             i < need_closure->questions_count;

test/TestAnswerContext.cc
@@ -164,6 +164,311 @@ TEST(TestAnswerContext, AnswerSuccessCallsNeedCallback) {
     EXPECT_EQ(1U, need_callback_called);
 }
 
+// Ensures calling the enqueued item answer_callback-s of
+// two b_answer_context_need_one calls with an answer calls
+// the b_answer_context_need_one success callback.
+TEST(TestAnswerContext, AnswerSuccessSuccessCallsNeedCallbacks) {
+    B_ErrorHandler const *eh = nullptr;
+
+    // Must be alive while question_queue is destructed.
+    StrictMock<MockQuestion> question;
+    MockRefCounting(question);
+    StrictMock<MockQuestion> needed_question_1;
+    MockRefCounting(needed_question_1);
+    StrictMock<MockQuestion> needed_question_2;
+    MockRefCounting(needed_question_2);
+    StrictMock<MockAnswer> answer_1;
+    MockRefCounting(answer_1);
+    StrictMock<MockAnswer> answer_2;
+    MockRefCounting(answer_2);
+    B_AnswerContext answer_context;
+
+    B_QuestionQueue *question_queue_raw;
+    ASSERT_TRUE(b_question_queue_allocate(
+        &question_queue_raw,
+        eh));
+    std::unique_ptr<B_QuestionQueue, B_QuestionQueueDeleter>
+        question_queue(question_queue_raw, eh);
+    question_queue_raw = nullptr;
+
+    answer_context.question = &question;
+    answer_context.question_vtable = MockQuestion::vtable();
+    answer_context.answer_callback = [](
+            B_Answer *,
+            void *,
+            B_ErrorHandler const *) {
+        return true;
+    };
+    answer_context.answer_callback_opaque = nullptr;
+    answer_context.question_queue = question_queue.get();
+    answer_context.dependency_delegate = nullptr;
+
+    size_t need_callback_1_called = 0;
+    ASSERT_TRUE(b_answer_context_need_one(
+        &answer_context,
+        &needed_question_1,
+        MockQuestion::vtable(),
+        [&](
+                B_TRANSFER B_Answer *cur_answer,
+                B_ErrorHandler const *cur_eh) {
+            need_callback_1_called += 1;
+            EXPECT_EQ(&answer_1, cur_answer);
+            EXPECT_EQ(eh, cur_eh);
+            return true;
+        },
+        [](
+                B_ErrorHandler const *) {
+            ADD_FAILURE();
+            return true;
+        },
+        eh));
+
+    size_t need_callback_2_called = 0;
+    ASSERT_TRUE(b_answer_context_need_one(
+        &answer_context,
+        &needed_question_2,
+        MockQuestion::vtable(),
+        [&](
+                B_TRANSFER B_Answer *cur_answer,
+                B_ErrorHandler const *cur_eh) {
+            need_callback_2_called += 1;
+            EXPECT_EQ(&answer_2, cur_answer);
+            EXPECT_EQ(eh, cur_eh);
+            return true;
+        },
+        [](
+                B_ErrorHandler const *) {
+            ADD_FAILURE();
+            return true;
+        },
+        eh));
+
+    {
+        B_QuestionQueueItemObject *queue_item_1_raw;
+        ASSERT_TRUE(b_question_queue_try_dequeue(
+            question_queue.get(),
+            &queue_item_1_raw,
+            eh));
+        ASSERT_NE(nullptr, queue_item_1_raw);
+        std::unique_ptr<
+                B_QuestionQueueItemObject,
+                B_QuestionQueueItemDeleter>
+            queue_item_1(queue_item_1_raw, eh);
+        queue_item_1_raw = nullptr;
+        EXPECT_EQ(
+            MockQuestion::vtable(),
+            queue_item_1->question_vtable);
+        ASSERT_NE(nullptr, queue_item_1->answer_callback);
+
+        B_Answer *queue_item_1_answer;
+        size_t *queue_item_1_need_callback_called;
+        if (queue_item_1->question == &needed_question_1) {
+            queue_item_1_answer = &answer_1;
+            ASSERT_TRUE(B_RETAIN(&answer_1, eh));
+            queue_item_1_need_callback_called
+                = &need_callback_1_called;
+        } else if (queue_item_1->question
+                == &needed_question_2) {
+            queue_item_1_answer = &answer_2;
+            ASSERT_TRUE(B_RETAIN(&answer_2, eh));
+            queue_item_1_need_callback_called
+                = &need_callback_2_called;
+        } else {
+            FAIL();
+        }
+        EXPECT_EQ(0U, *queue_item_1_need_callback_called);
+        ASSERT_TRUE(queue_item_1->answer_callback(
+            queue_item_1_answer,
+            queue_item_1.get(),
+            eh));
+        EXPECT_EQ(1U, *queue_item_1_need_callback_called);
+    }
+
+    {
+        B_QuestionQueueItemObject *queue_item_2_raw;
+        ASSERT_TRUE(b_question_queue_try_dequeue(
+            question_queue.get(),
+            &queue_item_2_raw,
+            eh));
+        ASSERT_NE(nullptr, queue_item_2_raw);
+        std::unique_ptr<
+                B_QuestionQueueItemObject,
+                B_QuestionQueueItemDeleter>
+            queue_item_2(queue_item_2_raw, eh);
+        queue_item_2_raw = nullptr;
+        EXPECT_EQ(
+            MockQuestion::vtable(),
+            queue_item_2->question_vtable);
+        ASSERT_NE(nullptr, queue_item_2->answer_callback);
+
+        B_Answer *queue_item_2_answer;
+        size_t *queue_item_2_need_callback_called;
+        if (queue_item_2->question == &needed_question_1) {
+            queue_item_2_answer = &answer_1;
+            ASSERT_TRUE(B_RETAIN(&answer_1, eh));
+            queue_item_2_need_callback_called
+                = &need_callback_1_called;
+        } else if (queue_item_2->question
+                == &needed_question_2) {
+            queue_item_2_answer = &answer_2;
+            ASSERT_TRUE(B_RETAIN(&answer_2, eh));
+            queue_item_2_need_callback_called
+                = &need_callback_2_called;
+        } else {
+            FAIL();
+        }
+        EXPECT_EQ(0U, *queue_item_2_need_callback_called);
+        ASSERT_TRUE(queue_item_2->answer_callback(
+            queue_item_2_answer,
+            queue_item_2.get(),
+            eh));
+        EXPECT_EQ(1U, *queue_item_2_need_callback_called);
+    }
+}
+
+// Ensures calling the enqueued item answer_callback-s of a
+// b_answer_context_need call with an answer calls the
+// b_answer_context_need success callback.
+TEST(TestAnswerContext, AnswerSuccessSuccessCallsNeedCallback) {
+    B_ErrorHandler const *eh = nullptr;
+
+    // Must be alive while question_queue is destructed.
+    StrictMock<MockQuestion> question;
+    MockRefCounting(question);
+    StrictMock<MockQuestion> needed_question_1;
+    MockRefCounting(needed_question_1);
+    StrictMock<MockQuestion> needed_question_2;
+    MockRefCounting(needed_question_2);
+    StrictMock<MockAnswer> answer_1;
+    MockRefCounting(answer_1);
+    StrictMock<MockAnswer> answer_2;
+    MockRefCounting(answer_2);
+    B_AnswerContext answer_context;
+
+    B_QuestionQueue *question_queue_raw;
+    ASSERT_TRUE(b_question_queue_allocate(
+        &question_queue_raw,
+        eh));
+    std::unique_ptr<B_QuestionQueue, B_QuestionQueueDeleter>
+        question_queue(question_queue_raw, eh);
+    question_queue_raw = nullptr;
+
+    answer_context.question = &question;
+    answer_context.question_vtable = MockQuestion::vtable();
+    answer_context.answer_callback = [](
+            B_Answer *,
+            void *,
+            B_ErrorHandler const *) {
+        return true;
+    };
+    answer_context.answer_callback_opaque = nullptr;
+    answer_context.question_queue = question_queue.get();
+    answer_context.dependency_delegate = nullptr;
+
+    B_Question const *needed_questions[2] = {
+        &needed_question_1,
+        &needed_question_2,
+    };
+    B_QuestionVTable const *needed_question_vtables[2] = {
+        MockQuestion::vtable(),
+        MockQuestion::vtable(),
+    };
+
+    size_t need_callback_called = 0;
+    ASSERT_TRUE(b_answer_context_need(
+        &answer_context,
+        needed_questions,
+        needed_question_vtables,
+        2,
+        [&](
+                B_TRANSFER B_Answer *const *cur_answers,
+                B_ErrorHandler const *cur_eh) {
+            need_callback_called += 1;
+            EXPECT_EQ(&answer_1, cur_answers[0]);
+            EXPECT_EQ(&answer_2, cur_answers[1]);
+            EXPECT_EQ(eh, cur_eh);
+            return true;
+        },
+        [](
+                B_ErrorHandler const *) {
+            ADD_FAILURE();
+            return true;
+        },
+        eh));
+
+    {
+        B_QuestionQueueItemObject *queue_item_1_raw;
+        ASSERT_TRUE(b_question_queue_try_dequeue(
+            question_queue.get(),
+            &queue_item_1_raw,
+            eh));
+        ASSERT_NE(nullptr, queue_item_1_raw);
+        std::unique_ptr<
+                B_QuestionQueueItemObject,
+                B_QuestionQueueItemDeleter>
+            queue_item_1(queue_item_1_raw, eh);
+        queue_item_1_raw = nullptr;
+        EXPECT_EQ(
+            MockQuestion::vtable(),
+            queue_item_1->question_vtable);
+        ASSERT_NE(nullptr, queue_item_1->answer_callback);
+
+        B_Answer *queue_item_1_answer;
+        if (queue_item_1->question == &needed_question_1) {
+            queue_item_1_answer = &answer_1;
+            ASSERT_TRUE(B_RETAIN(&answer_1, eh));
+        } else if (queue_item_1->question
+                == &needed_question_2) {
+            queue_item_1_answer = &answer_2;
+            ASSERT_TRUE(B_RETAIN(&answer_2, eh));
+        } else {
+            FAIL();
+        }
+        EXPECT_EQ(0U, need_callback_called);
+        ASSERT_TRUE(queue_item_1->answer_callback(
+            queue_item_1_answer,
+            queue_item_1.get(),
+            eh));
+        EXPECT_EQ(0U, need_callback_called);
+    }
+
+    {
+        B_QuestionQueueItemObject *queue_item_2_raw;
+        ASSERT_TRUE(b_question_queue_try_dequeue(
+            question_queue.get(),
+            &queue_item_2_raw,
+            eh));
+        ASSERT_NE(nullptr, queue_item_2_raw);
+        std::unique_ptr<
+                B_QuestionQueueItemObject,
+                B_QuestionQueueItemDeleter>
+            queue_item_2(queue_item_2_raw, eh);
+        queue_item_2_raw = nullptr;
+        EXPECT_EQ(
+            MockQuestion::vtable(),
+            queue_item_2->question_vtable);
+        ASSERT_NE(nullptr, queue_item_2->answer_callback);
+
+        B_Answer *queue_item_2_answer;
+        if (queue_item_2->question == &needed_question_1) {
+            queue_item_2_answer = &answer_1;
+            ASSERT_TRUE(B_RETAIN(&answer_1, eh));
+        } else if (queue_item_2->question
+                == &needed_question_2) {
+            queue_item_2_answer = &answer_2;
+            ASSERT_TRUE(B_RETAIN(&answer_2, eh));
+        } else {
+            FAIL();
+        }
+        EXPECT_EQ(0U, need_callback_called);
+        ASSERT_TRUE(queue_item_2->answer_callback(
+            queue_item_2_answer,
+            queue_item_2.get(),
+            eh));
+        EXPECT_EQ(1U, need_callback_called);
+    }
+}
+
 // Ensures calling b_answer_context_need_one's enqueued
 // item answer_callback without an answer calls the
 // b_answer_context_need_one error callback.