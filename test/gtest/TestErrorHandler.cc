89bf3b02683b4f061f9fe8fa3bfdddbb69e10918
Matthew Glazar <strager@fb.com>
2015-02-13 19:16:06 -0800

    Simplify TestErrorHandler
    Increase compatibility by not relying on C++11 library
    features (std::function).

--- test/gtest/TestErrorHandler.cc
+++ test/gtest/TestErrorHandler.cc
@@ -1,37 +1,23 @@
 #include <B/Error.h>
 
-#include <functional>
+#include <errno.h>
 #include <gtest/gtest.h>
-
-struct FunctionErrorHandler_ :
-        public B_ErrorHandler {
-    typedef B_ErrorHandlerResult FunctionType(B_Error);
-
-    explicit
-    FunctionErrorHandler_(
-            std::function<FunctionType> func) :
-            B_ErrorHandler{error_handler_func},
-            func(func) {
-    }
-
-    std::function<FunctionType> func;
-
-private:
-    static B_ErrorHandlerResult
-    error_handler_func(
-            B_ErrorHandler const *error_handler,
-            B_Error error) {
-        return static_cast<FunctionErrorHandler_ const *>(
-            error_handler)->func(error);
-    }
-};
 
 TEST(TestErrorHandler, RaiseCallsErrorHandler)
 {
-    size_t call_count = 0;
-    FunctionErrorHandler_ error_handler([&](
+    struct TestErrorHandler_ : public B_ErrorHandler {
+        TestErrorHandler_(
+                B_ErrorHandlerFunc *f) :
+                B_ErrorHandler{f},
+                call_count(0) {
+        }
+        size_t mutable call_count;
+    };
+    TestErrorHandler_ error_handler([](
+            B_ErrorHandler const *eh,
             B_Error error) {
-        call_count += 1;
+        static_cast<TestErrorHandler_ const *>(eh)
+            ->call_count += 1;
         EXPECT_EQ(EINVAL, error.errno_value);
         return B_ERROR_RETRY;
     });
@@ -39,5 +25,5 @@ TEST(TestErrorHandler, RaiseCallsErrorHandler)
     EXPECT_EQ(B_ERROR_RETRY, b_raise_error(
         &error_handler,
         B_Error{EINVAL}));
-    EXPECT_EQ(1U, call_count);
+    EXPECT_EQ(1U, error_handler.call_count);
 }
