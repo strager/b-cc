2ad0f4501f99df38e7fb27e62f79bb3b3ff9a6eb
Matthew Glazar <strager@fb.com>
2015-03-29 17:12:15 -0700

    leak tracker

--- python/b/foreign/ref.py
+++ python/b/foreign/ref.py
@@ -1,6 +1,22 @@
+import os
+
+should_dump_leaked_objects \
+    = bool(os.getenv('B_PYTHON_DUMP_LEAKED_OBJECTS', False))
+
 try:
+    if should_dump_leaked_objects:
+        # refcpython does not support dump_leaked_objects.
+        raise ImportError()
+    import b.foreign.refcpython as ref
     from b.foreign.refcpython import incref
     from b.foreign.refcpython import decref
 except ImportError:
-    from b.foreign.refportable import incref
-    from b.foreign.refportable import decref
+    import b.foreign.refportable as ref
+
+incref = ref.incref
+decref = ref.decref
+
+if should_dump_leaked_objects:
+    import atexit
+    atexit.register(ref.dump_leaked_objects)
+    ref.track_tracebacks = True
--- python/b/foreign/refportable.py
+++ python/b/foreign/refportable.py
@@ -2,12 +2,27 @@
 Portable (and slow) implementations of incref and decref.
 '''
 import collections
+import os.path
+import sys
+import traceback
+
+# Manually set this to True to record a traceback every
+# incref and decref call.
+track_tracebacks = False
 
 # TODO(strager): Locking.
 _objects = collections.defaultdict(int)
+_tracebacks = collections.defaultdict(list)
+
+def _capture_traceback():
+    return traceback.extract_stack()
 
 def incref(object):
-    _objects[object] += 1
+    old_ref_count = _objects[object]
+    ref_count = old_ref_count + 1
+    _objects[object] = ref_count
+    if track_tracebacks:
+        _tracebacks[object].append(_capture_traceback())
 
 def decref(object):
     old_ref_count = _objects[object]
@@ -15,4 +30,53 @@ def decref(object):
     ref_count = old_ref_count - 1
     _objects[object] = ref_count
     if ref_count == 0:
+        del _tracebacks[object]
         del _objects[object]
+    else:
+        if track_tracebacks:
+            _tracebacks[object].append(_capture_traceback())
+
+def _last_index_of(pred, xs):
+    index = None
+    for (i, x) in enumerate(xs):
+        if pred(x):
+            index = i
+    return index
+
+def _print_traceback(tb, out):
+    records = list(tb)
+    # Skip the last two entires (_capture_traceback and
+    # incref/decref).
+    records.pop()
+    records.pop()
+    # Drop unittest framework stuff.
+    def is_unittest_record(frame):
+        (path, line, method, code) = frame
+        return (
+            '{_}unittest{_}'.format(_=os.path.sep) in path
+            and method == 'run'
+            and 'testMethod()' in code
+        )
+    unittest_index = _last_index_of(
+        is_unittest_record,
+        records,
+    )
+    if unittest_index is not None:
+        records = records[unittest_index + 1:]
+        out.write('  ...\n')
+    for item in traceback.format_list(records):
+        out.write(item)
+
+def dump_leaked_objects(out=sys.stderr):
+    out.write('{} objects leaked:\n'.format(len(_objects)))
+    for (object, refcount) in _objects.items():
+        out.write(' - {} (+{})\n'.format(
+            repr(object),
+            refcount,
+        ))
+        tracebacks = _tracebacks.get(object, None)
+        if tracebacks is not None:
+            for tb in tracebacks:
+                _print_traceback(tb=tb, out=out)
+                out.write('\n')
+            out.write('\n')
