00212834fa02ae95a199d24344ae0ef1c059ec0b
Matthew Glazar <strager@fb.com>
2015-05-09 16:27:21 -0700

    Wrap duplicated Python target and test CMake code
    Create b_add_python_executable and b_add_python_test, which
    are similar to add_executable and add_test but for Python
    instead of C.
    
    This will make it easier to write more Python tests.

---
+++ CMake/b_python.cmake
@@ -0,0 +1,30 @@
+include("${CMAKE_CURRENT_LIST_DIR}/b_copy_file.cmake")
+
+function (B_ADD_PYTHON_EXECUTABLE NAME SOURCE)
+  get_filename_component(SOURCE_NAME "${SOURCE}" NAME)
+  set(DEST "${CMAKE_CURRENT_BINARY_DIR}/${SOURCE_NAME}")
+  b_copy_file("${SOURCE}" "${DEST}")
+  add_custom_target(
+    "${NAME}"
+    ALL
+    DEPENDS "${DEST}"
+    SOURCES "${SOURCE}"
+  )
+  set_target_properties(
+    "${NAME}"
+    PROPERTIES
+    LOCATION "${DEST}"
+    OUTPUT_NAME "${SOURCE_NAME}"
+  )
+endfunction ()
+
+function (B_ADD_PYTHON_TEST NAME)
+  get_target_property(LOCATION "${NAME}" LOCATION)
+  add_test(NAME "${NAME}" COMMAND "${LOCATION}")
+  add_dependencies("${NAME}" b_RunTestWrapper)
+  set_property(
+    TEST "${NAME}"
+    APPEND PROPERTY
+    ENVIRONMENT "B_RUN_TEST_SH=${B_RUN_TEST_WRAPPER}"
+  )
+endfunction ()
