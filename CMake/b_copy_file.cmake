095bcbe94d3f469689bc4a6bf978c8df83d4f15e
Matthew Glazar <strager@fb.com>
2015-05-09 17:10:25 -0700

    Make b_copy_file copy absolute paths
    Make b_copy_file work better with relative paths by
    making copied paths absolute at the call to b_copy_file.
    This fixes some future calls.

--- CMake/b_copy_file.cmake
+++ CMake/b_copy_file.cmake
@@ -1,7 +1,9 @@
 function (B_COPY_FILE SOURCE DEST)
+  get_filename_component(ABS_SOURCE "${SOURCE}" ABSOLUTE)
+  get_filename_component(ABS_DEST "${DEST}" ABSOLUTE)
   add_custom_command(
     OUTPUT "${DEST}"
-    COMMAND cmake -E copy "${SOURCE}" "${DEST}"
+    COMMAND cmake -E copy "${ABS_SOURCE}" "${ABS_DEST}"
     DEPENDS "${SOURCE}"
     COMMENT "Copying ${SOURCE}"
   )
